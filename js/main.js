e5200d6e6280fb740da5616b34f1e4f29b838683
Dominic Fawcett <Dominic.Fawcett@jacobs.com>
2019-12-11 16:33:59 +0000

    Corrected dodgy SVG images

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 1200) {
-        $('#custom-nav').addClass('affix');
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-        $('.navbar-brand img').attr('src','./img/logo/logo-poweredbyjacobs.svg');
-    } else {
-        $('#custom-nav').removeClass('affix');
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-        $('.navbar-brand img').attr('src','./img/logo/logo-placeholder.svg')
-    }   
-});
+// $(window).scroll(function() {
+//     if ($(".navbar").offset().top > 1200) {
+//         $('#custom-nav').addClass('affix');
+//         $(".navbar-fixed-top").addClass("top-nav-collapse");
+//         $('.navbar-brand img').attr('src','./img/logo/logo-poweredbyjacobs.svg');
+//     } else {
+//         $('#custom-nav').removeClass('affix');
+//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+//         $('.navbar-brand img').attr('src','./img/logo/logo-placeholder.svg')
+//     }   
+// });
 
 (function($) {
     "use strict"; // Start of use strict
