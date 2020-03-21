ade4adf21b0ce229cab7a0b8f0ce992b4a71690a
Dominic Fawcett <Dominic.Fawcett@jacobs.com>
2020-03-21 01:52:46 +0000

    New version updates to replace coming soon page

--- js/main.js
+++ js/main.js
@@ -1,48 +1,43 @@
-// $(window).scroll(function() {
-//     if ($(".navbar").offset().top > 1200) {
-//         $('#custom-nav').addClass('affix');
-//         $(".navbar-fixed-top").addClass("top-nav-collapse");
-//         $('.navbar-brand img').attr('src','./img/logo/logo-poweredbyjacobs.svg');
-//     } else {
-//         $('#custom-nav').removeClass('affix');
-//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
-//         $('.navbar-brand img').attr('src','./img/logo/logo-placeholder.svg')
-//     }   
-// });
+$(document).ready(function () {
+    $('#youtubeVideo').on('hidden.bs.modal', function () {
+        var $this = $(this).find('iframe'),
+            tempSrc = $this.attr('src');
+        $this.attr('src', "");
+        $this.attr('src', tempSrc);
+    });
 
-$(document).ready(function() {
+    $('#vidModal').on('hidden.bs.modal', function () {
+        var html5Video = document.getElementById("heroVideo");
+        if (html5Video != null) {
+            html5Video.pause();
+            html5Video.currentTime = 0;
+        }
+    });
+    $('#vidModal').on('shown.bs.modal', function () {
+        var html5Video = document.getElementById("heroVideo");
+        if (html5Video != null) {
+            html5Video.play();
+            html5Video.currentTime = 0;
+        }
+    });
+});
 
-  // Gets the video src from the data-src on each button
-  
-  var $videoSrc;  
-  $('.video-btn').click(function() {
-      $videoSrc = $(this).data( "src" );
-  });
-  console.log($videoSrc);
-  
-    
-    
-  // when the modal is opened autoplay it  
-  $('#vidModal').on('shown.bs.modal', function (e) {
-      
-  // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-  $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
-  })
-    
-  
-  
-  // stop playing the youtube video when I close the modal
-  $('#vidModal').on('hide.bs.modal', function (e) {
-      // a poor man's stop video
-      $("#video").attr('src',$videoSrc); 
-  }) 
-      
-      
-  
-  
+var textOverImages = document.getElementsByClassName("onClickTextOverImage");
+var previousTextOverImage;
+for (var i = 0; i < textOverImages.length; i++) {
+    textOverImages[i].onclick = function () {
+        var classes = this.classList;
+        if (classes.contains("show")) {
+            classes.remove("show");
+        } else {
+            if (previousTextOverImage != null)
+                previousTextOverImage.classList.remove("show");
+            previousTextOverImage = this;
+            classes.add("show");
+        }
+    }
+}
 
-  // document ready  
-  });
-  
-  
-  
+function stopPropagation(event) {
+    event.stopPropagation();
+}
